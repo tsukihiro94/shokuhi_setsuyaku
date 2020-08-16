0b00661b9c07c60d3d48211ae743635e1773c7a6
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-16 20:05:12 +0900

    expensesコントローラー生成

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,7 @@
+class ExpensesController < ApplicationController
+
+  def index
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get 'posts/index'
   devise_for :users
   root "posts#index"
+  resources :expenses
 end
