ee499bd6ba062ff18e35b9d17a3324467a2f6f7d
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-17 20:34:26 +0900

    カテゴリーのactive_hash実装

---
+++ app/models/category.rb
@@ -0,0 +1,13 @@
+class Category < ActiveHash::Base
+self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '朝食' },
+    { id: 3, name: '昼食' },
+    { id: 4, name: '夕食' },
+    { id: 5, name: '食材' },
+    { id: 6, name: '飲み物' },
+    { id: 7, name: 'おやつ' },
+    { id: 8, name: '交際費' },
+    { id: 9, name: 'その他' }
+]
+end
