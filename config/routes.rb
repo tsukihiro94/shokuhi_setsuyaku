d9218dfb996cf70bfad17202bf1dd8bc6c7d2339
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-08-21 16:30:01 +0900

    日別のページのビューなどの設計

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'posts/index'
   devise_for :users
   root "posts#index"
-  resources :expenses do
+  resources :expenses, only:[:show,:create,:new,:destroy ] do
     collection do
       get 'search'
     end
