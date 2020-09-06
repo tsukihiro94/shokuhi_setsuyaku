805d65ac0ca0e4700f4689a2aec7e8f34a78a51a
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-09-06 14:51:26 +0900

    ユーザーログインの結合テスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe "user", type: :system do
   before do
     @user = FactoryBot.build(:user)
   end
@@ -55,4 +55,49 @@ RSpec.describe "Users", type: :system do
       expect(current_path).to eq "/users"
     end
   end
+end
+  RSpec.describe 'ログイン', type: :system do
+    before do
+      @user = FactoryBot.create(:user)
+    end
+    context 'ログインができるとき' do
+      it '保存されているユーザーの情報と合致すればログインができる' do
+        # トップページに移動する
+        visit root_path
+        # トップページにログインページへ遷移するボタンがあることを確認する
+        expect(page).to have_content('ログイン')
+        # ログインページへ遷移する
+        visit new_user_session_path
+        # 正しいユーザー情報を入力する
+        fill_in 'user[email]', with: @user.email
+        fill_in 'user[password]', with: @user.password
+        # ログインボタンを押す
+        find('input[name="commit"]').click
+        # トップページへ遷移することを確認する
+        expect(current_path).to eq root_path
+        # ログアウトボタンとマイページボタンが表示されることを確認する
+        expect(page).to have_content('ログアウト')
+        expect(page).to have_content('マイページ')
+        # 新規登録ボタンとログインボタンが表示されていないことを確認する
+        expect(page).to have_no_content('新規登録')
+        expect(page).to have_no_content('ログイン')
+      end
+    end
+    context 'ログインができないとき' do
+      it '保存されているユーザーの情報と合致しないとログインができない' do
+        # トップページに移動する
+        visit root_path
+        # トップページにログインページへ遷移するボタンがあることを確認する
+        expect(page).to have_content('ログイン')
+        # ログインページへ遷移する
+        visit new_user_session_path
+        # ユーザー情報を入力する
+        fill_in 'user[email]', with:""
+        fill_in 'user[password]', with: ""
+        # ログインボタンを押す
+        find('input[name="commit"]').click
+        # ログインページへ戻されることを確認する
+        expect(current_path).to eq "/users/sign_in"
+      end
+    end
 end
