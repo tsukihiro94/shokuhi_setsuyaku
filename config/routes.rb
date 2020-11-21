8b48f8a83b4bcba3a2306ca5761798bdd1ee36fb
tsukihiro94 <tsukishima.hiromu@gmail.com>
2020-11-21 10:44:19 +0900

    ゲスト用のコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
+  post '/posts/guest_sign_in', to: 'posts#new_guest'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
